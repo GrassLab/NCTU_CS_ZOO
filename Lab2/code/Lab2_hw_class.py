4be444ed980240d84ba98a6b5e1e5a6ec4b0456e
李東霖 <lee097019@gmail.com>
2020-09-25 18:27:17 +0800

    Add Lab2 HW, maze_info.txt and hw_class.py

---
+++ Lab2/code/Lab2_hw_class.py
@@ -0,0 +1,17 @@
+import time
+from reprint import output
+
+class maze():
+	def __init__(self, size):
+		self.size = size
+		self.cur_pos = [0, 0]
+		self.cur_maze = []
+
+	def gen_map(self, obstacles):
+		# TODO: generate each map & saved in self.cur_maze
+		...
+
+	def show_maze(self):
+		# TODO: show all the maps
+		...
+
