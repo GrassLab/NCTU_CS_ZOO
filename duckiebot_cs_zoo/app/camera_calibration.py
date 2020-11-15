8ea8c4e88816d684247396ade1fb3b79e11fa63c
李東霖 <lee097019@gmail.com>
2020-11-15 22:17:37 +0800

    Update tool under app

--- duckiebot_cs_zoo/app/camera_calibration.py
+++ duckiebot_cs_zoo/app/camera_calibration.py
@@ -2,68 +2,73 @@ import numpy as np
 import cv2, os, joblib
 import os.path as osp
 from utils.fileutils import check_path
-from nodes.duckiebot_client import DuckiebotClient
+from libs.camera_control import CameraControl
+from os import listdir
+from os.path import isfile
 
 
-def camera_calibration_chessboard(duckiebot, square_sz, board_size, file_path):
+def camera_calibration_chessboard(camera, square_sz, board_size, folder_path, intrinsics_name="intrinsics.pkl"):
     """
     Please take around 20-30 images from varies angles, positions(left-right , far-near)
-
-    :param duckiebot: DuckiebotClient object
+    :param camera: CameraControl object
     :param square_sz: Exact size of one square in the chessboard pattern (unit in meter)
     :param board_size: tuple (W,H), number of squares (black and white) for each row and column
-    :param file_path: intrinsics save file path
+    :param folder_path: image and intrinsics.pkl save folder path
     :return: intrinsic
     """
-    os.putenv('DISPLAY', ':0')
-    check_path(osp.dirname(file_path))
-    h, w = duckiebot.get_stream_img().shape[:2]
-    # Chessboard Mapping
+    check_path(folder_path)
+    image_path = osp.join(folder_path, "image")
+    check_path(image_path)
+    file_list = [f for f in listdir(image_path) if isfile(osp.join(image_path, f))]
+
     corner_u, corner_v = board_size[0] - 1, board_size[1] - 1
     objpoints = np.zeros((corner_u * corner_v, 3), dtype=np.float32)
     objpoints[:, 0:2] = np.mgrid[0:corner_u, 0:corner_v].T.reshape(-1, 2) * square_sz  # (su-1)x(sv-1)x3
     world_points = []
     img_points = []
-    print(
-        'Collecting frames from camera,press 1 to collect, q to finish. Display shows once the Chessboard is detected')
-    while True:
-        rgb = duckiebot.get_stream_img()
+
+    print(f"Calibrate camera using {len(file_list)} images...")
+    for f in file_list:
+        if f[-3:] != "png" and f[-3:] != "jpg":
+            continue
+        rgb = cv2.imread(osp.join(image_path, f))
+        h, w = rgb.shape[:2]
         gray = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
         ret, corners = cv2.findChessboardCornersSB(gray, (corner_u, corner_v), flags=cv2.CALIB_CB_ACCURACY)
-        vis = cv2.drawChessboardCorners(rgb, (corner_u, corner_v), corners, ret)
-        cv2.imshow("Chessboard Corner", vis)
-        k = cv2.waitKey(30)
-        if k == ord('1') and ret:
+        if ret:
+            # Ensure that all corner-arrays are going from top to bottom.
+            # ref: https://github.com/ros-perception/image_pipeline/blob/noetic/camera_calibration/src/camera_calibration/calibrator.py#L214
+            if corners[0, 0, 1] > corners[-1, 0, 1]:
+                corners = np.copy(np.flipud(corners))
             world_points.append(objpoints)
             img_points.append(corners)
-            print('Pattern {} saved'.format(len(world_points)))
-        elif k == ord('q'):
-            if len(world_points) < 20:
-                print('Please collect at least 20 images')
-            else:
-                break
 
+    if len(world_points) < 20:
+        print('Please collect at least 20 images on the precious step!')
+        return False
     rms, camera_matrix, dist_coeffs, _, _ = cv2.calibrateCamera(world_points, img_points, (w, h),
                                                                 None, None)
     print("Calibrated: rms={:5f}".format(rms))
     print('Intrinsic=\n{}\n Dist coeffs=\n{}'.format(camera_matrix, dist_coeffs))
-    joblib.dump((rms, camera_matrix, dist_coeffs), file_path)
+    joblib.dump((rms, camera_matrix, dist_coeffs), osp.join(folder_path, intrinsics_name))
     # A good result should have RMS lower than 0.5
     if rms > 0.7:
         print('Please do the calibration again')
+        return False
+    return True
 
 
-def validate_calibration(duckiebot, square_sz, board_size, file_path, alpha=0, draw_chessboard=False):
+def validate_calibration(camera, square_sz, board_size, folder_path, intrinsics_name="intrinsics.pkl", alpha=0, draw_chessboard=False):
     """
     Validate (streaming the un-distort images)
-    :param duckiebot: DuckiebotClient object
+    :param camera: CameraControl object
     :param square_sz: Exact size of one square in the chessboard pattern (unit in meter)
     :param board_size: tuple (W,H), number of squares for each row and column
-    :param file_path: intrinsics save file_path
+    :param folder_path: intrinsics save folder_path
     :param alpha
     """
-    _, camera_matrix, dist_coeffs = joblib.load(file_path)
-    h, w = duckiebot.get_stream_img().shape[:2]
+    _, camera_matrix, dist_coeffs = joblib.load(osp.join(folder_path, intrinsics_name))
+    h, w = camera.get_stream_img().shape[:2]
     """
     # alpha=0: image is scaled and rectified so that all pixels are un-distroted (no black areas)
     # alpha=1: retain all pixels in the original image (with black areas)
@@ -71,9 +76,12 @@ def validate_calibration(duckiebot, square_sz, board_size, file_path, alpha=0, d
     """
     newcameramtx, roi = cv2.getOptimalNewCameraMatrix(camera_matrix, dist_coeffs, (w, h), alpha, (w, h))
     mapx, mapy = cv2.initUndistortRectifyMap(camera_matrix, dist_coeffs, None, newcameramtx, (w, h), 5)
+    if roi == (0, 0, 0, 0):
+        print("Fail to calibrate camera, return to the previous step...")
+        return
 
     while True:
-        img = duckiebot.get_stream_img()
+        img = camera.get_stream_img()
         dst = cv2.remap(img, mapx, mapy, cv2.INTER_CUBIC)
 
         # # crop the image
@@ -85,7 +93,6 @@ def validate_calibration(duckiebot, square_sz, board_size, file_path, alpha=0, d
             gray = cv2.cvtColor(dst_crop, cv2.COLOR_BGR2GRAY)
             ret, corners = cv2.findChessboardCornersSB(gray, (corner_u, corner_v))
             dst_crop = cv2.drawChessboardCorners(dst_crop, (corner_u, corner_v), corners, ret)
-        cv2.imshow('Un-distort', dst)
         cv2.imshow('Rectified', dst_crop)
         k = cv2.waitKey(20)
         if ord('q') == k:
@@ -93,31 +100,12 @@ def validate_calibration(duckiebot, square_sz, board_size, file_path, alpha=0, d
 
 
 def run_cam_calib():  # Camera calibration
-    DEFAULT_DATA_PATH = osp.join(osp.dirname(osp.abspath(__file__)), 'data', 'camera_calibration')
-    file_path = osp.join(DEFAULT_DATA_PATH, 'intrinsics.pkl')
-    duckiebot = DuckiebotClient('duckiebotchai.local', 54761)
-    camera_calibration_chessboard(duckiebot, 0.031, (8, 6), file_path)
-    validate_calibration(duckiebot, 0.031, (8, 6), file_path, alpha=0, draw_chessboard=True) # compare alpha=0/1
-
-    # Write calibration data to the duckiebot client
-    rms, camera_matrix, dist_coeffs = joblib.load(file_path)
-    # Uncomment to save the result
-    # duckiebot.set_cam_calibration(camera_matrix, dist_coeffs, 480, 640)
-
-
-# Check rectified images
-def check_rectified():
-    duckiebot = DuckiebotClient('duckiebotchai.local', 54761, restore_data=True)
-    while True:
-        img = duckiebot.get_stream_img()
-        rect = duckiebot.get_rectified_image(img)
-        cv2.imshow('Rect', rect)
-        k = cv2.waitKey(20)
-        if k != -1:
-            if ord('q') == k:
-                break
+    folder_path = osp.join(osp.dirname(osp.abspath(__file__)), 'data', 'camera_calibration')
+    camera = CameraControl()
+    if camera_calibration_chessboard(camera, 0.031, (8, 6), folder_path):
+        validate_calibration(camera, 0.031, (8, 6), folder_path, alpha=0, draw_chessboard=False) # compare alpha=0/1
+    camera.stop_stream()
 
 
 if __name__ == '__main__':
     run_cam_calib()
-    check_rectified()
